4038eb6ebe8fffcb129afeae8b89460bba5fc489
Maksym Zayats <zmr2k17@gmail.com>
2026-02-06 23:45:38 +0200

    add sidebar, filter forms: city, type, size, inclusive

--- src/main.py
+++ src/main.py
@@ -5,14 +5,21 @@ import streamlit as st
 
 raw_dataset = dp.get_Bombshelter_info()
 geo_data = dp.get_normalize_data()
-#display(raw_dataset)
 
-#st.header("Таблиця всіх будівель цивільного захисту на території Закарпатської області")
-#st.dataframe(geo_data)
-cityName = st.sidebar.selectbox(
+st.sidebar.header("Filter")
+cityName: str = st.sidebar.selectbox(
     "Виберіть місто в якому бажаєте знайти укриття",
     dp.get_cityName(geo_data)
 )
+typeBombshelter : list = st.sidebar.multiselect("Тип укриття", ["Найпростіші укриття", "ПРУ", "Сховище"], default=["Найпростіші укриття", "ПРУ", "Сховище"] ) 
+
+size : int = st.sidebar.slider(
+    "Місткість бомбосховища",
+    0, 3000
+)
+bezbar : bool = st.sidebar.checkbox("Безбарʼєрність")
+
+
 st.header("Мапа будівель цивільного захисту на території Закарпатської області")
 st.write(cityName)
 st.map(dp.get_city_info(cityName, geo_data))
