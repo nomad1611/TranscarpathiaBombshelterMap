65ea38b22d8eea49dce584facf4c17859373d718
Maksym Zayats <zmr2k17@gmail.com>
2026-02-15 02:37:39 +0200

    Web: finish sidebar and filtering function,show  only filtered dataon the map; create second page for table Ref: rename main.py and table.py

--- src/1_🏠︎_Main.py
+++ src/1_🏠︎_Main.py
@@ -4,54 +4,72 @@ from IPython.display import display
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.set_page_config(page_title="Головна сторінка")
+st.set_page_config(page_title="Головна сторінка", page_icon="🏠")
 
 st.header("Головна сторінка")
 st.write("Інформаційно-аналітична система контролю та візуалізації будівель цивільного захисту на території Закарпатської області")
 st.subheader("Мапа")
+
 geo_data = dp.get_normalize_data()
 df_b = dp.get_extended_data(geo_data)
 
 
 
 
-m = leafmap.Map(center=[48.63176, 24], zoom=8)
+st.sidebar.title("Фільтр та Пошук")
+
+otg_options = [" "] + dp.get_sorted_columnData(df_b["ОТГ"])
+OTGName: str = st.sidebar.selectbox(
+    "ОТГ(об'єднана територіальна громада)",
+    otg_options
+)
+
+
+city_options = dp.get_city_info(OTGName, geo_data)
+cityName: str = st.sidebar.selectbox(
+    "Населений пункт",
+    city_options
+)
+
+
+Type = dp.get_sorted_columnData(df_b['Тип'])
+typeBombshelter : list = st.sidebar.multiselect("Тип укриття", Type, default = Type ) 
+
+
+size : int = st.sidebar.slider(
+   "Місткість бомбосховища",
+  0, 3876, 3876, 10
+)
+
+bezbar : bool = st.sidebar.checkbox("Безбарʼєрність")
+
+
+m = leafmap.Map(center = [48.63176, 24], zoom = 8)
 
 m.add_basemap('HYBRID')
 m.add_basemap('Stadia.StamenTerrainLines')
 m.add_basemap('Stadia.StamenTerrainLabels')
 m.add_basemap('Stadia.OSMBright',False)
 
-df_b['ID']=df_b.index
+
+df_point = dp.search_data(
+    df_b,
+    cityName,
+    OTGName,
+    typeBombshelter,
+    size,  
+    bezbar
+    )
+df_point['ID'] = df_point.index
+
 m.add_points_from_xy(
-   df_b,
+   df_point,
   x = 'longitude',
   y = 'latitude',
   popup=['ID','Назва', 'ОТГ', 'Населений пункт', 'Адреса','Тип','Місткість', 'Інклюзивність','Посилання'], 
-  
-    
-  )
+)
 
 m.to_streamlit()
-
-st.sidebar.header("Фільтр та Пошук")
-OTGName: str = st.sidebar.selectbox(
-    "ОТГ(об'єднана територіальна громада)",
-    dp.get_sorted_columnData("ОТГ", df_b)
-)
-cityName: str = st.sidebar.selectbox(
-    "Населений пункт",
-    dp.get_sorted_columnData("Населений пункт", df_b)
-)
-Type = dp.get_sorted_columnData('Тип', df_b)
-typeBombshelter : list = st.sidebar.multiselect("Тип укриття", Type, default = Type ) 
-
-size : int = st.sidebar.slider(
-   "Місткість бомбосховища",
-  0, 3876, 3876, 10
-)
-bezbar : bool = st.sidebar.checkbox("Безбарʼєрність")
-
 
 
  
--- src/pages/2_🗂️_Table.py
+++ src/pages/2_🗂️_Table.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import data_processing as dp
 
-st.set_page_config(page_title="Таблиця")
+st.set_page_config(page_title="Таблиця", page_icon="🗂️")
 st.sidebar.header("Таблиця")
 geo_data = dp.get_normalize_data()
 df_b = dp.get_extended_data(geo_data)
